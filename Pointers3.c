ad26268ad9f038ac16c78eb2b73412c24f9073ab
ammarm7 <ammarm7@gmail.com>
2014-10-28 23:15:45 -0400

    Added class on manipulating pointer values

---
+++ Pointers3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+
+	int x = 10;
+	int *p;
+
+	p = &x;
+
+    //Printing out details of X relationship with p
+	printf("The value of x:                %d\n", x);
+	printf("The address of x:              %p\n", &x);
+	printf("The address of x by pointer:   %p\n", p);
+	printf("The value of p:                %d\n", *p); //dereferenceing value
+	printf("The value of x:                %d\n", *&x); //dereferenced value
+
+	//Changing value by dereferenceing pointer
+	*p = 20;
+	printf("The value of x:  %d\n", *p);
+	printf("The value of x:  %d\n", x); //In both cases X was changed
+
+
+	return 0;
+}
